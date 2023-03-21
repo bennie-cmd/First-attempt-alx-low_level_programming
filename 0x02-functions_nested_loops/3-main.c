dc6aae6606fd8af5efb5635363fc375363a0d5ee
bennie <bennetotieno@gmail.com>
2023-03-21 14:58:26 -0700

    task 3 done

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -1,12 +1,14 @@
 #include "main.h"
+
 /**
-* main - check the code.
-*
-* Return: Always 0.
-*/
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 int r;
+
 r = _islower('H');
 _putchar(r + '0');
 r = _islower('o');
