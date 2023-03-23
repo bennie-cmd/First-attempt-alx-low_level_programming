aa5d89c27149de62bd239a2c8f1130959cddf98a
bennie <bennetotieno@gmail.com>
2023-03-23 13:41:03 -0700

    code for the lagest number

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * largest_number - Returns the largest of 3 numbers
+ * @a: First integer
+ * @b: Second integer
+ * @c: Third integer
+ *
+ * Return: Largest number
+ */
+int largest_number(int a, int b, int c)
+{
+int largest;
+
+if (a > b && b > c)
+{
+largest = a;
+}
+else if (b > a && a > c)
+{
+largest = b;
+}
+else
+{
+largest = c;
+}
+
+return (largest);
+
+}
